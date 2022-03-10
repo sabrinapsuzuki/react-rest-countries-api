6a86bc5f143a7f7b196dfe991cb566aba69495f4
sabrinapsuzuki <sabrinasuzuki@yahoo.com>
2022-03-10 20:47:30 -0300

    SelectFilter: adiconando seleçao no select

--- src/components/SelectFilter.jsx
+++ src/components/SelectFilter.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
+
 export const SelectFilter = () => {
+  const [region, setRegion] = useState("0");
+
   return (
-    <select
-      name="select"
-      class="w-1/2 sm:w-auto p-3 focus:outline-none shadow-lg border rounded-md"
-    >
-      <option selected disabled value="" className="">
-        Filter By Region
-      </option>
-      <option value="valor1">teste 1</option>
-      <option value="valor2">teste 2</option>
-    </select>
+    <form action="">
+      <label htmlFor=""></label>
+      <select
+        class="w-1/2 sm:w-auto p-3 focus:outline-none shadow-lg border rounded-md"
+        name="region"
+        value={region}
+        onChange={(event) => {
+          console.log(event.target.value);
+          setRegion(event.target.value);
+        }}
+      >
+        <option disabled value="0">
+          Filter By Region
+        </option>
+        <option value="america">America</option>
+        <option value="europe">Europe</option>
+      </select>
+    </form>
   );
 };
